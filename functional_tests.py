f3c94f1b52fc031e89136ea4c4ee44e69f1cb605
Ben <dia_v3@rambler.ru>
2023-07-06 18:28:18 +0300

    Базовое представление теперь возвращает минимальный HTML

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'The install worked successfully' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """ тест нового посетителя """
+
+    def setUp(self):
+        """ установка """
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        """ снос """
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """ тест можно начать список и получить его позже """
+        self.browser.get("http://localhost:8000")
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Закончить тест!")
+
+
+if __name__ == "__main__":
+    unittest.main()
